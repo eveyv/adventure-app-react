0c32613a76fcecba0fdf80a4c88e8d9f30af83f4
Everett Yeaw <evyeaw@gmail.com>
2020-05-14 13:31:44 -0400

    created states and destinations tables

---
+++ app/controllers/api/v1/destinations_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::DestinationsController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+
+  def index
+    render json: Destination.all
+  end
+
+  def show
+    destination = Destination.find(params[:id])
+    render json: destination
+  end
+
+  def create
+    destination = Destination.new(destination_params)
+    if destination.save
+      render json: { destinations: destination }
+    else
+      render json: { error: destination.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def destination_params
+    params.permit(:id, :name, :state, :type, :address, :activities, :cost, :price, :website)
+  end
+
+end
---
+++ app/controllers/api/v1/states_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::StatesController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+
+  def index
+    render json: State.all
+  end
+
+  def show
+    state = State.find(params[:id])
+    render json: destination
+  end
+end
