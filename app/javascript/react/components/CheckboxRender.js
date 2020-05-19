c076aa4f736ee2209fc2312441b556ba05b62e40
Everett Yeaw <evyeaw@gmail.com>
2020-05-18 20:55:56 -0400

    removed select/deselect/save

--- app/javascript/react/components/CheckboxRender.js
+++ app/javascript/react/components/CheckboxRender.js
@@ -68,26 +68,6 @@ class CheckboxRender extends Component {
           <div className="col-sm-12">
             <form onSubmit={this.handleFormSubmit}>
               {this.createCheckboxes()}
-
-              <div className="form-group mt-2">
-                <button
-                  type="button"
-                  className="btn btn-outline-primary mr-2"
-                  onClick={this.selectAll}
-                >
-                  Select All
-                </button>
-                <button
-                  type="button"
-                  className="btn btn-outline-primary mr-2"
-                  onClick={this.deselectAll}
-                >
-                  Deselect All
-                </button>
-                <button type="submit" className="btn btn-primary">
-                  Save
-                </button>
-              </div>
             </form>
           </div>
         </div>
