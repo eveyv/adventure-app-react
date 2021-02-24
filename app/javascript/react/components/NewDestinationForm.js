636032606deb6f592522d2d328372bd9841b57b4
Everett Yeaw <evyeaw@gmail.com>
2021-02-24 17:21:49 -0500

    fixed form ui, values update

--- app/javascript/react/components/NewDestinationForm.js
+++ app/javascript/react/components/NewDestinationForm.js
@@ -1,7 +1,6 @@
 import React, { useState }  from 'react'
 import _ from "lodash"
 import ErrorList from "./ErrorList"
-import CheckboxRender from './CheckboxRender'
 
 const NewDestinationForm = props => {
   const [ errors, setErrors ] = useState({})
@@ -59,79 +58,94 @@ const NewDestinationForm = props => {
   return(
     <form className="destination-form" onSubmit={handleSubmit}>
       <ErrorList errors={errors} />
-          <label className="form-text">
-            * Location Name:
-            <input
-            id="name"
+      <label className="form-text">
+        * Location Name:
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={newDestination.name}
+          onChange={handleInputChange}
+        />
+      </label>
+      <label className="form-text">
+        * State:
+          <select
+            id="state"
+            placeholder="Please select a state."
+            name="state"
+            value={newDestination.state}
+            onChange={handleInputChange}>
+              <option value="Connecticut">Connecticut</option>
+              <option value="Maine">Maine</option>
+              <option value="Massachusetts">Massachusetts</option>
+              <option value="New Hampshire">New Hampshire</option>
+              <option value="Rhode Island">Rhode Island</option>
+              <option value="Vermont">Vermont</option>
+          </select>
+        </label>
+        <label className="form-text">
+        * Activity:
+          <select
+            id="state"
+            placeholder="Select an activity"
+            name="state"
+            value={newDestination.activities}
+            onChange={handleInputChange}>
+              <option value="Snowmobiling">Snowmobiling</option>
+              <option value="Dog-walking">Dog-walking</option>
+              <option value="Off-Roading">Off-roading</option>
+              <option value="Boating">Boating</option>
+              <option value="Hiking">Hiking</option>
+              <option value="Running">Running</option>
+              <option value="Skiing">Skiing</option>
+              <option value="Other">Skiing</option>
+          </select>
+        </label>
+        <label className="form-text">
+          * Access Point (Street Address):
+          <input
+            id="address"
             type="text"
-            name="name"
-            value=""
+            name="address"
+            value={newDestination.address}
             onChange={handleInputChange}
+          />
+        </label>
+          <label className="form-text">
+            Is there a fee?
+            <input
+              id="cost"
+              type="text"
+              name="cost"
+              value={newDestination.cost}
+              onChange={handleInputChange}
             />
           </label>
-          <CheckboxRender
-            value={this.value}
-          />
           <label className="form-text">
-            * State:
-              <select
-                id="state"
-                placeholder="Please select a state."
-                name="state"
-                value={newDestination.state}
-                onChange={handleInputChange}>
-                  <option value="Connecticut">Connecticut</option>
-                  <option value="Maine">Maine</option>
-                  <option value="Massachusetts">Massachusetts</option>
-                  <option value="New Hampshire">New Hampshire</option>
-                  <option value="Rhode Island">Rhode Island</option>
-                  <option value="Vermont">Vermont</option>
-              </select>
-            </label>
-            <label className="form-text">
-              * Access Point (Street Address):
-              <input
-                id="address"
-                type="text"
-                name="address"
-                value={newDestination.address}
-                onChange={handleInputChange}
-              />
-            </label>
-              <label className="form-text">
-                Is there a fee?
-                <input
-                  id="cost"
-                  type="text"
-                  name="cost"
-                  value={newDestination.cost}
-                  onChange={handleInputChange}
-                />
-              </label>
-              <label className="form-text">
-                If there is a charge, how much is it?
-                <input
-                  id="price"
-                  type="text"
-                  name="price"
-                  value={newDestination.price}
-                  onChange={handleInputChange}
-                />
-              </label>
-              <label className="form-text">
-                Link to website?
-                <input
-                  id="website"
-                  type="text"
-                  name="website"
-                  value={newDestination.website}
-                  onChange={handleInputChange}>
-                </input>
-              </label>
-              <input
-                type="submit"
-                className="btn"
-              />
+            If there is a charge, how much is it?
+            <input
+              id="price"
+              type="text"
+              name="price"
+              value={newDestination.price}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label className="form-text">
+            Link to website?
+            <input
+              id="website"
+              type="text"
+              name="website"
+              value={newDestination.website}
+              onChange={handleInputChange}>
+            </input>
+          </label>
+          <input
+            type="submit"
+            className="btn"
+          />
         </form>
 
   )
