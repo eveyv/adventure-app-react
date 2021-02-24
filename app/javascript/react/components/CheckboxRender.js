944ad314ab4b5c5cee0ef184f733865216e5a977
Everett Yeaw <evyeaw@gmail.com>
2021-02-24 15:04:24 -0500

    project up and running from mac to wsl2

--- app/javascript/react/components/CheckboxRender.js
+++ app/javascript/react/components/CheckboxRender.js
@@ -4,63 +4,8 @@ const OPTIONS = [ "Hiking", "Boating/Kayaking", "Camping", "Dog Park", "Jogging"
 
 let activitiesArray = []
 
-class CheckboxRender extends Component {
-  state = {
-    checkboxes: OPTIONS.reduce(
-      (options, option) => ({
-        ...options,
-        [option]: false
-      }),
-      {}
-    )
-  };
-
-  handleCheckboxChange = changeEvent => {
-    const { name } = changeEvent.target;
-    this.setState(prevState => ({
-      checkboxes: {
-        ...prevState.checkboxes,
-        [name]: !prevState.checkboxes[name]
-      }
-    }));
-  };
-
-  handleFormSubmit = formSubmitEvent => {
-    formSubmitEvent.preventDefault();
-
-    Object.keys(this.state.checkboxes)
-      .filter(checkbox => this.state.checkboxes[checkbox])
-      .forEach(checkbox => {
-        activitiesArray << value
-      });
-  };
-
-
-  createCheckbox = option => (
-    <Checkbox
-      label={option}
-      isSelected={this.state.checkboxes[option]}
-      onCheckboxChange={this.handleCheckboxChange}
-      key={option}
-      onSubmit={this.handleFormSubmit}
-      id={option}
-      value={option}
-    />
-  );
-
-  createCheckboxes = () => OPTIONS.map(this.createCheckbox);
-
-  render() {
-    return (
-      <div className="container">
-        <div className="row mt-5">
-          <div className="col-sm-12">
-              {this.createCheckboxes()}
-          </div>
-        </div>
-      </div>
-    );
-  }
+class CheckboxList extends Component {
+  
 }
 
-export default CheckboxRender;
+export default CheckboxList;
