1e203612c4f44e20b17404d6208aad8a443eb222
Everett Yeaw <evyeaw@gmail.com>
2021-03-01 14:20:30 -0500

    updated background to image

--- app/javascript/react/components/Welcome.js
+++ app/javascript/react/components/Welcome.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../../../assets/stylesheets/welcome.scss'
+import '../../../assets/stylesheets/bg-img.scss'
+
 
 const Welcome = (props) => {
 
   return(
-    <div>
-      <h1 className="balance-home-header">Welcome!</h1>
-      <Link to={`/destinations/new`} className="link"> Add a Location</Link>
+      <div className="bg-img">
+        <h1 className="balance-home-header">Welcome!</h1>
+        <Link to={`/destinations/new`} className="link"> Add a Location</Link>
     </div>
   )
 }
